f5be0d7e1e0b9de40c6415322350ab7c176159de
NQW1105 <neoqw@hotmail.com>
2023-01-27 21:29:37 +0800

    Restructure directories and begin work on state for 'Experience' component

---
+++ src/components/EditJob.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class EditJob extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default EditJob;
---
+++ src/components/JobForm.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class JobForm extends Component {
+  render() {
+    return null;
+  }
+}
+
+export default JobForm;
